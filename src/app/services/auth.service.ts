ffde4e773c925aee798b9de30111b7cbc9de4bdc
unknown <edwi02@gmail.com>
2018-01-15 02:35:15 +0100

    Obteniendo información de la autenticación

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,13 +8,15 @@ import * as auth0 from 'auth0-js';
 @Injectable()
 export class AuthService {
 
+  public userProfile:any;
+
   auth0 = new auth0.WebAuth({
     clientID: '5ciEDwqXiq3rx5bd4yqViME9px3z0cM7',
     domain: 'edwinqr.eu.auth0.com',
     responseType: 'token id_token',
     audience: 'https://edwinqr.eu.auth0.com/userinfo',
     redirectUri: 'http://localhost:3000/callback',
-    scope: 'openid'
+    scope: 'openid profile'
   });
 
   constructor(public router: Router) {}
@@ -59,5 +61,20 @@ export class AuthService {
    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
    return new Date().getTime() < expiresAt;
  }
+
+ public getProfile(cb): void {
+    const accessToken = localStorage.getItem('access_token');
+    if (!accessToken) {
+      throw new Error('Access token must exist to fetch profile');
+    }
+
+    const self = this;
+    this.auth0.client.userInfo(accessToken, (err, profile) => {
+      if (profile) {
+        self.userProfile = profile;
+      }
+      cb(err, profile);
+    });
+  }
 
 }
